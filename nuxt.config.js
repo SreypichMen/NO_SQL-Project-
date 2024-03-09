6b6a4acf98025b4389686316c5e0d35e1b2659db
CYTech Student <sreypichmen364@gmail.com>
2024-03-09 17:33:40 +0100

    5th_4

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,5 +93,17 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console: true
+        }
+      }
+    }
+  },
+
+  generate: {
+      fallback: '404.html',
+  },
 }
